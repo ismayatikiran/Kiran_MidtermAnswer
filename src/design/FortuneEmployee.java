1b8e065a16cdfd17748c5a72fd1bcd2bdbaababd
ismayatikiran <37750299+ismayatikiran@users.noreply.github.com>
2018-04-10 16:11:52 -0400

    Desing Answers

--- src/design/FortuneEmployee.java
+++ src/design/FortuneEmployee.java
@@ -1,11 +1,16 @@
 package design;
 
+import databases.ConnectDB;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class FortuneEmployee {
 
 	/**
 	 * FortuneEmployee class has a main methods where you will be able to create Object from
-	 * EmployeeInfo class to use fields and attributes.Demonstrate as many methods as possible 
-	 * to use with proper business work flow.Think as a Software Architect, Product Designer and 
+	 * EmployeeInfo class to use fields and attributes.Demonstrate as many methods as possible
+	 * to use with proper business work flow.Think as a Software Architect, Product Designer and
 	 * as a Software Developer.(employee.info.system) package is given as an outline,you need to elaborate
 	 * more to design an application that will meet for fortune 500 Employee Information
 	 * Services.
@@ -13,10 +18,22 @@ public class FortuneEmployee {
 	 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 	 *
 	 **/
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws SQLException, IOException, ClassNotFoundException {
+		EmployeeInfo employee1 = new EmployeeInfo(2);
+		EmployeeInfo employee2 = new EmployeeInfo("Ismayati",34);
+		EmployeeInfo employee3 = new EmployeeInfo("Kiran",36,21);
+
+		employee1.setName("Ki");
+		System.out.println(employee1.getName());
+
+		employee2.setName("Kiran");
+		System.out.println(employee2.getName());
 
 
+		EmployeeInfo.calculateEmployeeBonus(126000,8);
+		EmployeeInfo.calculateEmployeePension(60000);
+
+		ConnectDB connection = new ConnectDB();
+		connection.insertProfileToMySql("employees","column1","column2");
 	}
-
 }
--- src/design/UnitTestingEmployeeInfo.java
+++ src/design/UnitTestingEmployeeInfo.java
@@ -1,8 +1,14 @@
 package design;
 
+import org.testng.Assert;
 public class UnitTestingEmployeeInfo {
     public static void main(String[] args) {
+
         //Write Unit Test for all the methods has been implemented in this package.
 
+        // EmployeeInfo refVar = new EmployeeInfo(101);
+        Assert.assertEquals(EmployeeInfo.calculateEmployeeBonus(9000,12),860);
+        Assert.assertEquals(EmployeeInfo.calculateEmployeePension(8000),4000.0);
+        Assert.assertEquals(100,100);
     }
 }
